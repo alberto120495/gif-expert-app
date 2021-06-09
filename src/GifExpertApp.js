a30b81957202ee20b02de5be6b87043477ae5d99
AlbertoPimentel <albertopimentel04@gmail.com>
2021-06-09 16:28:57 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
+
+function GifExpertApp() {
+  const [categories, setCategories] = useState(["goku"]);
+
+  /* const handleAdd = () => {
+    setCategories([...categories, "SAO"]);
+  }; */
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+}
+
+export default GifExpertApp;
---
+++ src/components/GifGrid.js
@@ -0,0 +1,25 @@
+// import { useEffect, useState } from "react";
+// import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import GifGridItem from "./GifGridItem";
+function GifGrid({ category }) {
+  const { data: images, loading } = useFetchGifs(category);
+  /* const [images, setImages] = useState([]);
+  useEffect(() => {
+    getGifs(category).then(setImages);
+  }, [category]);
+ */
+  return (
+    <>
+      <h3 className="animate__animated animate__fadeIn">{category}</h3>
+      {loading && <p className="animate__animated animate__flash">Loading</p>}
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifGridItem key={image.id} {...image} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default GifGrid;
